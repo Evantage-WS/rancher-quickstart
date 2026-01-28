1235a11bcbd1ef820dcaae858eb8e80fa6cfdfcc
Evantage-WS <info@evantage.nl>
2026-01-28 13:55:21 +0100

    bump version rancher

--- _common/suse_rancher/provider.tf
+++ _common/suse_rancher/provider.tf
@@ -2,15 +2,15 @@ terraform {
   required_providers {
     helm = {
       source                = "hashicorp/helm" # https://registry.terraform.io/providers/hashicorp/helm/latest
-      version               = "3.0.2"
+      version               = "3.1.1"
     }
     local = {
       source                = "hashicorp/local" # https://registry.terraform.io/providers/hashicorp/local/latest
-      version               = "2.5.3"
+      version               = "2.6.1"
     }
     rancher2                = {
       source                = "rancher/rancher2" # https://registry.terraform.io/providers/rancher/rancher2/latest
-      version               = "8.1.0"
+      version               = "13.1.4"
       configuration_aliases = [
         rancher2.admin,
         rancher2.bootstrap
